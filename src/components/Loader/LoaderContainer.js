a101fe396f1ea80bd4f8f41e3f8655a6b9bdeb54
Leeland Clenista <leeland_clenista@yahoo.com>
2019-05-06 16:50:27 -0700

    loader is now a smaller size

--- src/components/Loader/LoaderContainer.js
+++ src/components/Loader/LoaderContainer.js
@@ -2,7 +2,7 @@ import React from "react"
 import Loader from "react-loader-spinner"
 
 const LoadingSpinner = () => {
-  return <Loader type="Bars" color="#EA8840" height="350" width="350" />;
+  return <Loader type="Bars" color="#EA8840" height="20%" width="20%" />;
 };
 
 export default LoadingSpinner
